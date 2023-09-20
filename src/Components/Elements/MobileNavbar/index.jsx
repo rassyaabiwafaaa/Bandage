4c786b30a3b50cb6161072c69fe6b80c7fa2bfb4
rassyaabiwafa <rassya.abiwafa@gmail.com>
2023-09-21 02:03:26 +0700

    create mobile navbar

---
+++ src/Components/Elements/MobileNavbar/index.jsx
@@ -0,0 +1,5 @@
+export default function MobileNavbar(){
+    return(
+        <h1>this is mobile navbar</h1>
+    )
+}
--- src/Components/Elements/Navbar/index.jsx
+++ src/Components/Elements/Navbar/index.jsx
@@ -1,11 +1,19 @@
 import TopNavbar from "./TopNavbar"
 import BottomNavbar from "./BottomNavbar"
+import MobileNavbar from "../MobileNavbar"
 
 export default function Navbar(){
     return(
-        <div className="w-full h-[136px]">
+        <>
+        {/* Desktop Navbar */}
+        <div className="md:block hidden w-full h-[136px]">
             <TopNavbar/>
             <BottomNavbar/>
         </div>
+        {/* Mobile Navbar */}
+        <div className="block md:hidden">
+            <MobileNavbar/>
+        </div>
+        </>
     )
 }
